da0165eddfcb04306a2a41fa223ab6a46e30252b
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-28 15:10:14 +0530

    Add files via upload

--- Prac3/Prac3.py
+++ Prac3/Prac3.py
@@ -1,13 +1,6 @@
-n = int(input())
-
-room = map(int, input().split())
-room = sorted(room)
-
-
-for i in range(len(room)):
-    if i != len(room) - 1:
-        if room[i] != room[i + 1] and room[i] != room[i + 1]:
-            print(room[i])
-            break
-    else:
-        print(room[i])
+k = int(input())
+room_number_list = list(map(int, input().split()))
+room=set(room_number_list)
+for i in room:
+     if room_number_list.count(i)==1:
+         print(i)
